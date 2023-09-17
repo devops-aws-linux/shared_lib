28219c4a7fbb205100e1eff0e8979af4a584d61d
sivaram <ramsivak9@gmail.com>
2023-09-17 17:43:28 +0530

    make the chnages

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -1,5 +1,5 @@
 def call(Map stageParams){
-    checkout([
+    gitCheckout([
         $class: 'GitSCM',
         branches: [[name: stageParams.branch]],
         userRemoteConfigs: [[ url: stageParams.url]]
