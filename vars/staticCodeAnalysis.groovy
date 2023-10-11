6c9b5beb867462fee84f15dfde2862548ac8398f
sivaram <ramsivak9@gmail.com>
2023-10-11 16:26:13 +0530

    commit the changes

--- vars/staticCodeAnalysis.groovy
+++ vars/staticCodeAnalysis.groovy
@@ -1,3 +1,5 @@
-def call(){
-    sh 'mvn clean package sonar:sonar'
+def call(credentialsId){
+    withSonarQubeEnv(credentialsId: 'sonarcred') {
+        sh 'mvn clean package sonar:sonar'
+    }
 }
