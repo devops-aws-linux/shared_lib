47e499d69c46be7c1d0ea55e6fc33f95f8d0ccfe
sivaram <ramsivak9@gmail.com>
2023-10-17 15:54:53 +0530

    modified the code

--- vars/dpCheck.groovy
+++ vars/dpCheck.groovy
@@ -1,13 +1,31 @@
-def call(toolName, additionalArguments, reportPattern){
+// def call(toolName, additionalArguments, reportPattern){
+//     def additionalArgs = additionalArguments.trim()
+//     sh """
+//         ${toolName} \\
+//         ${additionalArgs} \\
+//         --odcInstallation=${toolName}
+//     """
+//     step([
+//         $class: 'DependencyCheckPublisher',
+//         pattern: reportPattern
+//     ])
+
+// }
+
+def callCustomFunction(params) {
+    def toolName = params.toolName
+    def additionalArguments = params.additionalArguments
+    def reportPattern = params.reportPattern
+
     def additionalArgs = additionalArguments.trim()
     sh """
         ${toolName} \\
         ${additionalArgs} \\
         --odcInstallation=${toolName}
     """
+    
     step([
         $class: 'DependencyCheckPublisher',
         pattern: reportPattern
     ])
-
 }
